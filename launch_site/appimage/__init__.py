b7bebdcfbb19659a12ac78dd37f9f39992fd9ae5
Yujeonja <hardboiled65@gmail.com>
2020-02-24 12:38:03 +0900

    Initial commit.

---
+++ launch_site/appimage/__init__.py
@@ -0,0 +1,100 @@
+import os
+import shutil
+import subprocess
+
+
+class AppImage:
+    def __init__(self, appimage_file):
+        self._file = appimage_file
+
+        # Get md5 of AppImage file.
+        md5sum = subprocess.run(
+            ['md5sum', appimage_file],
+            stdout=subprocess.PIPE
+        )
+        md5sum_result = md5sum.stdout.decode()
+        md5sum_result = md5sum_result.split()[0]
+
+        self._md5 = md5sum_result
+
+        # Get offset of AppImage.
+        appimage_offset = subprocess.run(
+            [appimage_file, '--appimage-offset'],
+            stdout=subprocess.PIPE
+        )
+        appimage_offset_result = appimage_offset.stdout.decode()
+
+        self._offset = int(appimage_offset_result)
+
+    def _make_squashfs(self):
+        f = open(self.file, 'rb')
+        squash = open('/tmp/appimage-' + self.md5 + '.squashfs', 'wb')
+        f.seek(self.offset)
+        read = f.read(1024)
+        while read != b'':
+            squash.write(read)
+            read = f.read(1024)
+        f.close()
+        squash.close()
+        return '/tmp/appimage-' + self.md5 + '.squashfs'
+
+    def _unsquashfs(self):
+        unsquash = subprocess.run([
+                'unsquashfs',
+                '-d',
+                '/tmp/appimage-' + self.md5,
+                '/tmp/appimage-' + self.md5 + '.squashfs'
+            ],
+            stdout=subprocess.PIPE
+        )
+        return '/tmp/appimage-' + self.md5
+
+    def _unmount(self, rmdir=True):
+        contents = os.listdir('/tmp/appimage-' + self.md5)
+        for i in contents:
+            file_to_delete = os.path.join('/tmp/appimage-' + self.md5, i)
+            if os.path.isfile(file_to_delete):
+                os.remove(file_to_delete)
+            if os.path.isdir(file_to_delete):
+                shutil.rmtree(file_to_delete)
+        if rmdir is False:
+            pass
+        os.removedirs('/tmp/appimage-' + self.md5)
+
+    def _delete_squashfs(self):
+        os.remove('/tmp/' + 'appimage-' + self.md5 + '.squashfs')
+
+    @property
+    def file(self):
+        return self._file
+
+    @property
+    def name(self):
+        return os.path.basename(self.file)
+
+    @property
+    def md5(self):
+        return self._md5
+
+    @property
+    def offset(self):
+        return self._offset
+
+    def save_metadata(self):
+        self._make_squashfs()
+        unsquashed = self._unsquashfs()
+
+        # Do save things.
+        desktop = None
+        icons = None
+        icon = None
+        # Copy .desktop file.
+        contents = os.listdir(unsquashed)
+        for i in contents:
+            if i.endswith('.desktop'):
+                desktop = i
+        # Copy icons.
+        # Copy icon.
+
+        self._unmount()
+        self._delete_squashfs()
---
+++ src/AppImage.py
@@ -0,0 +1,7 @@
+from PySide2.QtCore import QObject, Property
+
+
+class AppImage(QObject):
+    def __init__(self):
+        QObject.__init__(self)
+
